50deba49bcaa284ef5fb3e0193a8ad98674e48db
Asutosh0033 <ashutoshkumar300225@gmail.com>
2023-12-21 23:38:13 +0530

    push
    push

--- project.js
+++ project.js
@@ -1,2 +1,7 @@
-if(window.innerWidth<1060)
-        alert('For better experience open in Desktop');
+// if(window.innerWidth<1060)
+//         alert('For better experience open in Desktop');
+let menu = document.querySelector('.menu_bar');
+
+menu.addEventListener('click',()=>{
+    
+})
